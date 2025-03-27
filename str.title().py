8562b72de96f92257f9ff77faf70f2d28b01fef4
shadow-fist <dagidaw001@gmail.com>
2025-03-27 17:05:22 +0300

    first

---
+++ str.title().py
@@ -0,0 +1,3 @@
+text = "gOod MOrniNg"
+titlecase_text =text.title()
+print(titlecase_text)  # the Output is will be: "Good Morning"
---
+++ str.upper().py
@@ -0,0 +1,3 @@
+text = "gOod MOrniNg"
+uppercase_text = text.upper()
+print(uppercase_text)  # the Output is will be: "GOOD MORNING"
