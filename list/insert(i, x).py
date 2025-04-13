866815675eb1831919e40850d5cd0ae9cfe1917b
shadow-fist <dagidaw001@gmail.com>
2025-04-13 20:00:10 +0300

    day ten

---
+++ list/insert(i, x).py
@@ -0,0 +1,4 @@
+names = ['Alice', 'Charlie']
+names.insert(1, 'Bob')  # Insert 'Bob' at index 1
+
+print(names) #the output will be ['Alice', 'Bob', 'Charlie']
---
+++ list/pop([i]).py
@@ -0,0 +1,5 @@
+fruits = ['apple', 'banana', 'cherry']
+first_fruit = fruits.pop(0)
+
+print(first_fruit)  # apple
+print(fruits)       # ['banana', 'cherry']
